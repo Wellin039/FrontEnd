d29d912ef4bedca46e702146f49b741021ef1705
Wellington Ramos <Wellin039@gmail.com>
2023-07-26 21:54:32 -0300

    introdução JS

---
+++ Script.js
@@ -0,0 +1,22 @@
+/*
+    Git básico
+    Introdução Java Script
+*/
+
+var nome = "Wellington"
+let idade = 31
+const anoNascimento = 1991;
+let idadeEsposa = "31"
+/*
+= atribuição
+== comparar se é igual
+=== comparar se é exatamente igual (compara a informação e o tipo)
+*/
+console.log(idade === idadeEsposa);
+
+let listaCompras = ["Maça", "Manga", "Maracujá", 13, 17, 56.90]
+
+listaCompras.push("Maça")
+
+console.log(listaCompras.length)
+console.log(listaCompras)
