b105cbae9e6bd56bb5748fc5e690fb8190d18fac
Wellington Ramos <Wellin039@gmail.com>
2023-07-27 20:14:02 -0300

    aula 2

--- Script.js
+++ Script.js
@@ -12,11 +12,48 @@ let idadeEsposa = "31"
 == comparar se é igual
 === comparar se é exatamente igual (compara a informação e o tipo)
 */
+
 console.log(idade === idadeEsposa);
 
 let listaCompras = ["Maça", "Manga", "Maracujá", 13, 17, 56.90]
 
 listaCompras.push("Maça")
 
-console.log(listaCompras.length)
 console.log(listaCompras)
+
+const titulo = document.getElementById("h1")
+
+const  = document.getElementById("container")
+
+
+
+console.log(titulo)
+console.log(typeof(nome))
+
+switch (num) {
+    case 7:
+        console.log("Sete")
+        break;
+        case 5:
+            console.log("Cinco")
+            break;
+        
+}
+
+//Array
+/*
+acessamos o array fazendo referencia ao index
+*/
+
+let lista = [1,2,3,4,5,6]
+
+//&&
+//||
+//!
+
+for (let index = 0; index < lista.length; index++) {
+    const element = lista[index];
+
+    console.log(element)
+
+}
