b0fbbd886eb39e06654af5a82612d9279b9c7478
Wellington Ramos <Wellin039@gmail.com>
2023-07-27 21:46:39 -0300

    aula 2.1

--- Script.js
+++ Script.js
@@ -1,59 +1,15 @@
-/*
-    Git básico
-    Introdução Java Script
-*/
+const titulo = document.getElementById("titulo")
+const nomeInput = document.getElementById("nome")
+const email = document.getElementById("email")
+const telefone = document.getElementById("telefone")
+const resultado = document.getElementById("resultado")
 
-var nome = "Wellington"
-let idade = 31
-const anoNascimento = 1991;
-let idadeEsposa = "31"
-/*
-= atribuição
-== comparar se é igual
-=== comparar se é exatamente igual (compara a informação e o tipo)
-*/
+titulo.textContent = "Senai Cotia 2023"
 
-console.log(idade === idadeEsposa);
+function salvar() {
 
-let listaCompras = ["Maça", "Manga", "Maracujá", 13, 17, 56.90]
-
-listaCompras.push("Maça")
-
-console.log(listaCompras)
-
-const titulo = document.getElementById("h1")
-
-const  = document.getElementById("container")
-
-
-
-console.log(titulo)
-console.log(typeof(nome))
-
-switch (num) {
-    case 7:
-        console.log("Sete")
-        break;
-        case 5:
-            console.log("Cinco")
-            break;
-        
-}
-
-//Array
-/*
-acessamos o array fazendo referencia ao index
-*/
-
-let lista = [1,2,3,4,5,6]
-
-//&&
-//||
-//!
-
-for (let index = 0; index < lista.length; index++) {
-    const element = lista[index];
-
-    console.log(element)
+resultado.textContent = nomeInput.value
+resultado2.textContent = email.value
+resultado3.textContent = telefone.value
 
 }
